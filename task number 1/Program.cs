3d0a233e04878f3e60146d4867089cd6b24386b0
Vadim <suokasvrn@gmail.com>
2022-06-26 17:56:57 +0300

    first example solution

---
+++ task number 1/Program.cs
@@ -0,0 +1,18 @@
+﻿
+int a; int b; int max;
+
+Console.Write("Введите первое число ");
+a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число ");
+b = Convert.ToInt32(Console.ReadLine());
+
+if(a>b)
+{
+    max = a;
+    Console.WriteLine("Максимальное по значению число" + " " + max);  
+} 
+else
+{
+    max=b;
+    Console.WriteLine("Максимальное по значению число" + " " + max);
+}
