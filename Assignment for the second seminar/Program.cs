8d9f6c07c63224279ffb38b163c2bec132f451ae
Vadim <suokasvrn@gmail.com>
2022-06-27 23:24:38 +0300

    lesson two task ten

---
+++ Assignment for the second seminar/Program.cs
@@ -0,0 +1,15 @@
+﻿int SecondNumber()
+{
+    Console.WriteLine("Введите число от 100 до 1000 ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    
+
+    int sotni = num / 10;
+    int des = sotni % 10;
+
+    int result = des;
+    return result;
+}
+
+int s = SecondNumber();
+Console.Write("Результат" + " " + s);
