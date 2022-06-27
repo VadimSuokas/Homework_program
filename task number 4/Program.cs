6fbbcc282e1d1a878a59253a61b4d50b335f37ef
Vadim <suokasvrn@gmail.com>
2022-06-27 22:50:11 +0300

    Third example solution

---
+++ task number 4/Program.cs
@@ -0,0 +1,9 @@
+﻿int num; 
+
+Console.Write(" Введите число ");
+num = Convert.ToInt32(Console.ReadLine());
+
+if(num % 2 == 0)
+    Console.Write(" Число является четным ");
+else
+    Console.Write(" Число является нечетным ");
