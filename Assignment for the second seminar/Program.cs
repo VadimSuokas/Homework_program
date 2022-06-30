f547532e9b78ba81e0f28e932cee1a70e5cf1775
Vadim <suokasvrn@gmail.com>
2022-06-30 23:41:57 +0300

    task for the second seminar is solved

--- Assignment for the second seminar/Program.cs
+++ Assignment for the second seminar/Program.cs
@@ -1,4 +1,6 @@
-﻿int SecondNumber()
+﻿/*Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа. */
+/*
+int SecondNumber()
 {
     Console.WriteLine("Введите число от 100 до 1000 ");
     int num = Convert.ToInt32(Console.ReadLine());
@@ -12,4 +14,50 @@
 }
 
 int s = SecondNumber();
-Console.Write("Результат" + " " + s);
+Console.Write("Результат" + " " + s); */
+
+/*Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.*/
+/*
+int ThirdNumber()
+{
+    Console.WriteLine("Введите число ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    
+    if(num<100) Console.Write(" " + "Введено число в котором нет третьего символа");
+    while(num > 1000)
+    {
+        num = num / 10;
+    }    
+    int third = num % 10;
+    int result = third;
+    return result;  
+}
+
+int t = ThirdNumber();
+Console.Write(" " + t); */
+
+/*Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным. */
+
+/*
+int dayoftheweek(int d)
+{
+    
+    int result = 1;
+
+    if(d>=5 && d<8) result = -1;
+    if(d<5 && d>0)  result = 0;
+    return result;
+}
+
+   
+    Console.WriteLine("Введите число от 1 до 7 ");
+    int d = Convert.ToInt32(Console.ReadLine());
+
+    int v = dayoftheweek(d);
+    if(v == -1) Console.WriteLine("Выходной");
+    else Console.WriteLine("Рабочий");
+    */
+    
+
+    
+
