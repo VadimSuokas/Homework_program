acbdd4872995e77d82b127a3193303ff39468266
Vadim <suokasvrn@gmail.com>
2022-07-02 00:14:57 +0300

    second example solution

--- task of the third seminar/Program.cs
+++ task of the third seminar/Program.cs
@@ -20,8 +20,11 @@ Console.Write("Введите число ");
 palindrome();
 */
 
+/*Задача 23
 
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N. */
 
+/*
 void cube(int N)
 {
     int current = 1;
@@ -36,3 +39,4 @@ Console.Write("Введите число ");
 int N = Convert.ToInt32(Console.ReadLine());
 
 cube(N);
+*/
