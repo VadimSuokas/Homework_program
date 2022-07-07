d996979be57663e3d7b4c2e50823bcbcf8691486
Vadim <suokasvrn@gmail.com>
2022-07-08 00:16:50 +0300

    task 34

---
+++ task for the fifth seminar/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+int[] ArrayRandom(int size, int min, int max)
+{
+    int[] array = new int[size];
+    for(int i = 0; i < size; i++) 
+        if(min>99 && max<1000) array[i] = new Random().Next(min, max);
+
+    return array;
+}
+
+void ShowArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++) Console.Write(array[i] + " ");
+    Console.WriteLine();
+}
+
+int EvenNumbers(int[] Array)
+{
+    int count = 0;
+    for(int i = 0; i < Array.Length; i++) 
+    if(Array[i] % 2 == 0) count++;
+    return count;
+}
+
+
+Console.Write("Введите колличество элементов в массиве ");
+int size = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите начальное трехзначное число массива ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите конечное трехзначное число массива ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[] Array = ArrayRandom(size, min, max);
+
+ShowArray(Array);
+
+int even = EvenNumbers(Array);
+Console.WriteLine("Колличество четных чисел в массиве равно " + even);
+
+
+
+// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
