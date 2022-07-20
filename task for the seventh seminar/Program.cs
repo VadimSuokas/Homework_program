229ff93f9003247668cff4a12869bc134ac47160
Vadim <suokasvrn@gmail.com>
2022-07-20 19:07:21 +0300

    tasks solved

--- task for the seventh seminar/Program.cs
+++ task for the seventh seminar/Program.cs
@@ -1,5 +1,5 @@
 ﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
-/*
+
 double[,] RealNumbers(int rows, int columns, int minValue, int maxValue)
 {
     double[,] newArray = new double[rows, columns];
@@ -33,9 +33,9 @@ int max = Convert.ToInt32(Console.ReadLine());
 
 double[,] myArray = RealNumbers(m,n,min,max);
 ShowArrayRandom(myArray);
-*/
+
 // Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
-// P.S Долго думал как сделать чтобы результат else выводился только один раз, но к сожалению так и не придумал :(
+
 int[,] ArrayMatrix(int rows, int columns, int min, int max)
 {
     int[,] array = new int[rows,columns];
@@ -46,7 +46,7 @@ int[,] ArrayMatrix(int rows, int columns, int min, int max)
     
     return array;
 }
-void ShowArrayRandom(int[,] array)
+void ShowArrayRandom2d(int[,] array)
 {
     for(int i= 0; i < array.GetLength(0); i++)
     {
@@ -56,43 +56,72 @@ void ShowArrayRandom(int[,] array)
         Console.WriteLine();
     }
 }
-void ElementPosition(int[,] array, int element)
+
+void ElementPosition(int[,] array, int x, int y)
 {
+    if(x-1 > array.GetLength(0) || x-1 < 0 || y-1 > array.GetLength(1) || y-1 < 0)
+        Console.WriteLine("Элемента с заданными параметрами не существует в массиве ");
     for(int i = 0; i < array.GetLength(0); i++)
         for(int j = 0; j < array.GetLength(1); j++)
-            if(array[i,j] == element)
-                Console.WriteLine("Заданный элемент находится на " + (i+1) + " " + "строчке " + "в" + " " + (j+1) + " " + "столбце " );
-            else Console.WriteLine("Заданный элемент в массиве отстутствует ");
+            if(i == x - 1 && j == y - 1)
+                Console.WriteLine("На заданной позиции значение элемента " + array[i,j]);   
+           
 }
 
 Console.Write("Input number of rows ");
-int m = Convert.ToInt32(Console.ReadLine());
+int M = Convert.ToInt32(Console.ReadLine());
 Console.Write("Input number of columns ");
-int n = Convert.ToInt32(Console.ReadLine());
+int N = Convert.ToInt32(Console.ReadLine());
 Console.Write("Input number of value ");
-int min = Convert.ToInt32(Console.ReadLine());
+int Min = Convert.ToInt32(Console.ReadLine());
 Console.Write("Input number of value ");
-int max = Convert.ToInt32(Console.ReadLine());
+int Max = Convert.ToInt32(Console.ReadLine());
 
-int[,] Arr = ArrayMatrix(m,n,min,max);
-ShowArrayRandom(Arr);
-Console.Write("Input number ");
-int e = Convert.ToInt32(Console.ReadLine());
-ElementPosition(Arr,e);
+int[,] Arr = ArrayMatrix(M,N,Min,Max);
+ShowArrayRandom2d(Arr);
+Console.Write("Position in rows ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Position in columns ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+ElementPosition(Arr,x, y);
+
 // Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
-
-double[,] ArithmeticMean(int[,] array, int size)
+void ShowArrayRandom2(int[,] array)
 {
-    double[] Arr = new double[size];
-    double current = 0;
-    for(int i = 0; i < array.GetLength(0); i++)
-        for(int j = 0; j < array.GetLength(1); j++) 
-            Arr[i] = current/
-            while(j==0)
-            {
-                current += array[i,j];   
-            }
-
-
-
+    for(int i= 0; i < array.GetLength(0); i++)
+    {
+        for(int j= 0; j < array.GetLength(1); j++)
+            Console.Write(array[i,j] + " ");
+        
+        Console.WriteLine();
+    }
 }
+void ArithmeticMean(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        double srArif = 0;
+        for (int j = 0; j < array.GetLength(1); j++)
+            srArif += array[j, i];
+    
+        Console.WriteLine($"Cреднее арифметическое {i+1} столбца  = {srArif/array.GetLength(1)}");
+    }
+    
+}
+
+Console.Write("Input number of rows ");
+int m1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input number of columns ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input number of value ");
+int min1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input number of value ");
+int max1 = Convert.ToInt32(Console.ReadLine());
+
+int[,] Arr2d = ArrayMatrix(m1,n1,min1,max1);
+ShowArrayRandom2(Arr2d);
+Console.WriteLine();
+
+ArithmeticMean(Arr);
+
